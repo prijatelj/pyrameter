d1bb0b6d5a3917c64e4a14fb0b53e9691219527e
prijatelj <theomnipotentwoof@gmail.com>
2018-11-10 20:57:49 -0500

    another attempt at getting sort_methods accessible via import of the package

--- pyrameter/__init__.py
+++ pyrameter/__init__.py
@@ -2,5 +2,6 @@ from .build import build
 from .scope import Scope
 from .domain import ContinuousDomain, DiscreteDomain, ExhaustiveDomain
 from .modelgroup import ModelGroup
+import .sort_methods
 
-__all___ = ['Scope', 'ContinuousDomain', 'DiscreteDomain', 'ExhaustiveDomain', 'sort_methods']
+__all__ = ['Scope', 'ContinuousDomain', 'DiscreteDomain', 'ExhaustiveDomain', 'sort_methods']
--- pyrameter/sort_methods/__init__.py
+++ pyrameter/sort_methods/__init__.py
@@ -1,1 +1,5 @@
+import basic_sorts
+import genetic_algorithm
+import naive_bayes
+
 __all__ = ['basic_sort', 'genetic_algorithm', 'naive_bayes']
