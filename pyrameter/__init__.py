397693d7cf3932a009ba02eb6558bad2e2744f47
prijatelj <theomnipotentwoof@gmail.com>
2018-11-10 21:13:15 -0500

    removing sort_methods import and name from pyrameter/__init__.py

--- pyrameter/__init__.py
+++ pyrameter/__init__.py
@@ -2,6 +2,5 @@ from .build import build
 from .scope import Scope
 from .domain import ContinuousDomain, DiscreteDomain, ExhaustiveDomain
 from .modelgroup import ModelGroup
-from . import sort_methods
 
-__all__ = ['Scope', 'ContinuousDomain', 'DiscreteDomain', 'ExhaustiveDomain', 'sort_methods']
+__all__ = ['Scope', 'ContinuousDomain', 'DiscreteDomain', 'ExhaustiveDomain']
